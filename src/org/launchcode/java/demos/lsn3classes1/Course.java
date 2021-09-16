a79587ed93e55f917c68662a00e8f8532bde2833
unknown <kevinaborders@gmail.com>
2021-09-16 18:14:20 -0500

    class 4 exercises

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -6,5 +6,12 @@ public class Course {
         private String topic;
         private Teacher instructor;
         private ArrayList<Student> enrolledStudents;
+
+
+
+        public String toString() {
+                String courseDescription = String.format("%s is the class taught by %s with %d students enrolled", this.topic, this.instructor, this.enrolledStudents.size());
+                return courseDescription;
+        }
 }
 
